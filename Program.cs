c94b39611a68a245ec54052e97de0096857ce40b
unknown <nkosimova06@gmail.com>
2020-04-08 21:09:39 +0500

    Creating new class ArrayHelper, adding names of all methods with int arguments

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Poject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = {1,4,5,8,7,8,9,10};
+        }
+    }
+
+
+    //All methods constructed without using List
+    /*methods with int parametrs */
+    static class ArrayHelper 
+    {
+        public static int Pop(ref int[] array) 
+        {
+
+        }
+        public static int Push(ref int[] array, int new_elem)
+        {
+
+        }
+        public static int Shift(ref int[] array) 
+        {
+
+        }
+        public static int UnShift(ref int[] array, int new_elem)
+        {
+            
+        }
+
+
+
+    }
+ 
+}
